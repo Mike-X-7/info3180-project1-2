67f8490cbef92b1268afe2bce2866f9b8f8d6451
Michael Green <likemike11@yahoo.com>
2016-03-03 05:11:06 +0000

    PLEASE READ THE COMMENTS IN __init__

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,6 @@ db = SQLAlchemy(app)
 
 from app import views, models
 
-#
+# The app didn't write to my database(the link above) I got an error when running the "heroku run init" statement.
+# however, i tryed to link another database to the app and it WORKED PERFECTLY.
+# someone else's database i tried, worked:   postgres://nuvvubrysbqmrz:QjhlkvAnHI8QY_RETj76R8Q63-@ec2-23-21-219-209.compute-1.amazonaws.com:5432/d8rqq5trvho033
